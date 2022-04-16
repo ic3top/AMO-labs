029e4a2cd18811bc2c9be2c1a5f214d02c7ce4b8
ic3top <dvdvdv889@gmail.com>
2022-04-16 18:33:21 +0300

    Lab 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import "../node_modules/react-vis/dist/style.css";
 import {Menu} from "./components/Menu";
 import {
   BrowserRouter,
@@ -9,6 +10,7 @@ import {Lab2} from "./pages/lab2/Lab2";
 import {Linear} from "./pages/lab1/Linear";
 import {Branched} from "./pages/lab1/Branched";
 import {Circular} from "./pages/lab1/Circular";
+import {Lab3} from "./pages/lab3/Lab3";
 
 function App() {
   return (
@@ -23,7 +25,7 @@ function App() {
             <Route path="circular" element={<Circular />} />
           </Route>
           <Route path="/lab-2" element={<Lab2 />} />
-          <Route path="/lab-3" element={null} />
+          <Route path="/lab-3" element={<Lab3 />} />
           <Route path="/lab-4" element={null} />
           <Route path="/lab-5" element={null} />
           <Route path="*" element={<Navigate to="/lab-1" replace />} />
